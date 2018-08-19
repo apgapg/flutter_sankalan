268a1676ee06f2015d541b16f3dbe1a9b9d1d10c
ayushpguptaapg@gmail.com <Greens@123>
2018-08-19 20:30:04 +0530

    add fabric crashyltics

--- android/app/src/main/java/com/coddu/fluttersankalan/MainActivity.java
+++ android/app/src/main/java/com/coddu/fluttersankalan/MainActivity.java
@@ -3,11 +3,13 @@ package com.coddu.fluttersankalan;
 import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
-
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 public class MainActivity extends FlutterActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     GeneratedPluginRegistrant.registerWith(this);
+    Fabric.with(this, new Crashlytics());
   }
 }
